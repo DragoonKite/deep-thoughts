ac999b5770a5a8c8183fdd1c8dc63f0291cd7d59
DragoonKite <frank.depaolo@yahoo.com>
2021-05-10 17:27:45 -0500

    added mutations for adding throughts,reactions,and friends

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const {ApolloServer} = require('apollo-server-express');
 //Import our typeDefs and Resolvers
 const { typeDefs, resolvers } = require('./schemas');
 
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -12,7 +14,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context:authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
